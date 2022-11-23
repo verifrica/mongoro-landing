ec2dd10f077ad0138f22b8a36bc38f35fc2dd283
itzthem3ji <victorfeyitimi@gmail.com>
2022-11-23 13:36:58 +0100

    edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,28 +34,150 @@ const timeFunction = setInterval(()=>{
 }, 1000)
 
 
-function validation() {
-	let form = document.getElementById('form')
-	let email = document.getElementById('email').value
-	let text = document.getElementById('text')
-	let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
-  
-	if (email.match(pattern)) {
-	  form.classList.add('valid')
-	  form.classList.remove('invalid')
-	  text.innerHTML = "Your Email Address in valid"
-	  text.style.color = '#00ff00'
-	} else {
-	  form.classList.remove('valid')
-	  form.classList.add('invalid')
-	  text.innerHTML = "Please Enter Valid Email Address"
-	  text.style.color = '#ff0000'
-	}
-  
-	if (email == '') {
-	  form.classList.remove('valid')
-	  form.classList.remove('invalid')
-	  text.innerHTML = ""
-	  text.style.color = '#00ff00'
-	}
+// Input fields
+const email = document.getElementById('email');
+// Form
+const form = document.getElementById('form');
+// Validation colors
+const green = '#4CAF50';
+const red = '#F44336';
+
+// Handle form
+form.addEventListener('submit', function(event) {
+  // Prevent default behaviour
+  event.preventDefault();
+  if (
+    validateEmail()
+  ) {
+    const name = firstName.value;
+    const container = document.querySelector('div.container');
+    const loader = document.createElement('div');
+    loader.className = 'progress';
+    const loadingBar = document.createElement('div');
+    loadingBar.className = 'indeterminate';
+    loader.appendChild(loadingBar);
+    container.appendChild(loader);
+    setTimeout(function() {
+      const loaderDiv = document.querySelector('div.progress');
+      const panel = document.createElement('div');
+      panel.className = 'card-panel green';
+      const text = document.createElement('span');
+      text.className = 'white-text';
+      text.appendChild(
+        document.createTextNode(
+          `Thanks for joining our waiting list, we have included ${name} to our waiting list`
+        )
+      );
+      panel.appendChild(text);
+      container.replaceChild(panel, loaderDiv);
+    }, 1000);
   }
+});
+
+// Validators
+function validateEmail() {
+  if (checkIfEmpty(email)) return;
+  if (!containsCharacters(email, 5)) return;
+  return true;
+}
+// Utility functions
+function checkIfEmpty(field) {
+  if (isEmpty(field.value.trim())) {
+    // set field invalid
+    setInvalid(field, `${field.name} must not be empty`);
+    return true;
+  } else {
+    // set field valid
+    setValid(field);
+    return false;
+  }
+}
+function isEmpty(value) {
+  if (value === '') return true;
+  return false;
+}
+function setInvalid(field, message) {
+  field.className = 'invalid';
+  field.nextElementSibling.innerHTML = message;
+  field.nextElementSibling.style.color = red;
+}
+function setValid(field) {
+  field.className = 'valid';
+  field.nextElementSibling.innerHTML = '';
+  //field.nextElementSibling.style.color = green;
+}
+function checkIfOnlyLetters(field) {
+  if (/^[a-zA-Z ]+$/.test(field.value)) {
+    setValid(field);
+    return true;
+  } else {
+    setInvalid(field, `${field.name} must contain only letters`);
+    return false;
+  }
+}
+function meetLength(field, minLength, maxLength) {
+  if (field.value.length >= minLength && field.value.length < maxLength) {
+    setValid(field);
+    return true;
+  } else if (field.value.length < minLength) {
+    setInvalid(
+      field,
+      `${field.name} must be at least ${minLength} characters long`
+    );
+    return false;
+  } else {
+    setInvalid(
+      field,
+      `${field.name} must be shorter than ${maxLength} characters`
+    );
+    return false;
+  }
+}
+function containsCharacters(field, code) {
+  let regEx;
+  switch (code) {
+    case 1:
+      // letters
+      regEx = /(?=.*[a-zA-Z])/;
+      return matchWithRegEx(regEx, field, 'Must contain at least one letter');
+    case 2:
+      // letter and numbers
+      regEx = /(?=.*\d)(?=.*[a-zA-Z])/;
+      return matchWithRegEx(
+        regEx,
+        field,
+        'Must contain at least one letter and one number'
+      );
+    case 3:
+      // uppercase, lowercase and number
+      regEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/;
+      return matchWithRegEx(
+        regEx,
+        field,
+        'Must contain at least one uppercase, one lowercase letter and one number'
+      );
+    case 4:
+      // uppercase, lowercase, number and special char
+      regEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
+      return matchWithRegEx(
+        regEx,
+        field,
+        'Must contain at least one uppercase, one lowercase letter, one number and one special character'
+      );
+    case 5:
+      // Email pattern
+      regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return matchWithRegEx(regEx, field, 'Must be a valid email address');
+    default:
+      return false;
+  }
+}
+function matchWithRegEx(regEx, field, message) {
+  if (field.value.match(regEx)) {
+    setValid(field);
+    return true;
+  } else {
+    setInvalid(field, message);
+    return false;
+  }
+}
